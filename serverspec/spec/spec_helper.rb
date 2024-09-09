390c8d41b978718c6a14508450d506b39ef04bfc
Leeo-pro <mineva.aug@gmail.com>
2024-09-09 21:12:34 +0900

    lecture13/server_helper+specファイル見直し

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -1,44 +1,25 @@
-  # you configure your source control system to ignore this file.
-  config.example_status_persistence_file_path = "spec/examples.txt"
-
-  # Limits the available syntax to the non-monkey patched syntax that is
-  # recommended. For more details, see:
-  #   - http://rspec.info/blog/2012/06/rspecs-new-expectation-syntax/
-  #   - http://www.teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
-  #   - http://rspec.info/blog/2014/05/notable-changes-in-rspec-3/#zero-monkey-patching-mode
-  config.disable_monkey_patching!
-
-  # Many RSpec users commonly either run the entire suite or an individual
-  # file, and it's useful to allow more verbose output when running an
-  # individual spec file.
-  if config.files_to_run.one?
-    # Use the documentation formatter for detailed output,
-    # unless a formatter has already been configured
-    # (e.g. via a command-line flag).
-    config.default_formatter = "doc"
-  end
-
-  # Print the 10 slowest examples and example groups at the
-  # end of the spec run, to help surface which specs are running
-  # particularly slow.
-  config.profile_examples = 10
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = :random
-
-  # Seed global randomization in this process using the `--seed` CLI option.
-  # Setting this allows you to use `--seed` to deterministically reproduce
-  # test failures related to randomization by passing the same `--seed` value
-  # as the one that triggered the failure.
-  Kernel.srand config.seed
-=end
-
 require 'serverspec'
 
 # サーバーのタイプを設定 (ローカルでテストする場合)
 set :backend, :exec
 
+# RSpec の設定
+RSpec.configure do |config|
+  # テスト結果の永続化 (不要であればコメントアウト)
+  config.example_status_persistence_file_path = "spec/examples.txt"
+
+  # 推奨される非モンキーパッチング構文を使用
+  config.disable_monkey_patching!
+
+  # 単一のファイルを実行する場合にドキュメント形式で出力
+  if config.files_to_run.one?
+    config.default_formatter = "doc"
+  end
+
+  # 最も遅い 10 のテストを表示 (不要であればコメントアウト)
+  config.profile_examples = 10
+
+  # テストの実行順序をランダム化
+  config.order = :random
+  Kernel.srand config.seed
 end
