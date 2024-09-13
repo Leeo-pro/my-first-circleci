8d04a85b72f4e9cfe89013d7e5a80e3237db5b85
Leeo-pro <mineva.aug@gmail.com>
2024-09-13 18:58:09 +0900

    lecture13/serverspec見直し

--- serverspec/spec/cfn-lecture/sample_spec.rb
+++ serverspec/spec/cfn-lecture/sample_spec.rb
@@ -1,10 +1,6 @@
 puts "spec_helper loaded" if defined?(RSpec)
 require 'spec_helper'
 
-describe package('httpd'), :if => os[:family] == 'redhat' do
-    it { should be_installed }
-end
-
 listen_port = 80
 
 # 想定通りの AMI が使用されているか
@@ -20,15 +16,16 @@ end
 
 # nginxが実行中であるか
 describe service('nginx') do
-  it { should be_installed }
+  it { should be_running }
+  it { should be_enabled }
 end
 
 # 80番portが空いているか
 describe port(listen_port) do
-  it { should be_listening }
+  it { should be_listening.on('0.0.0.0').with('tcp') }
 end
 
 # 指定したポートでのcurlコマンドのテスト
-describe command("curl http://127.0.0.1:#{listen_port}/_plugin/head/ -o /dev/null -w \"%{http_code}\n\" -s") do
+describe command("curl http://127.0.0.1:#{listen_port} -o /dev/null -w \"%{http_code}\n\" -s") do
   its(:stdout) { should match /^200$/ }
 end
